5bce38ec64f336e0f35407fe49c574ca6b27c481
chenjunyu19 <1335546814@qq.com>
2017-03-18 14:05:05 +0800

    更改取最大公因数和最小公倍数的函数名

--- fractions.js
+++ fractions.js
@@ -3,7 +3,7 @@
  * @param {Number} n1 数1
  * @param {Number} n2 数2
  */
-function getMaxCommonFactor(n1, n2) {
+function gcd(n1, n2) {
   var tmp;
   while (n2 != 0) {
     tmp = n1 % n2;
@@ -17,9 +17,8 @@ function getMaxCommonFactor(n1, n2) {
  * 取两个数的最小公倍数
  * @param {Number} n1 数1
  * @param {Number} n2 数2
- * @returns {Number}
  */
-function getMinCommonMultiple(n1, n2) {
+function lcm(n1, n2) {
   var tmp = n1;
   while (tmp % n2 != 0) {
     tmp += n1;
@@ -57,7 +56,7 @@ function moveMinusToNumerator(f) {
  */
 function reductionofAFraction(f) {
   f = moveMinusToNumerator(f);
-  var maxCommonFactor = getMaxCommonFactor(Math.abs(f.numerator), Math.abs(f.denominator));
+  var maxCommonFactor = gcd(Math.abs(f.numerator), Math.abs(f.denominator));
   f.numerator /= maxCommonFactor;
   f.denominator /= maxCommonFactor;
   return f;
@@ -72,7 +71,7 @@ function reductionofFractionstoACommonDenominator(f1, f2) {
   var f = [];
   f1 = reductionofAFraction(f1);
   f2 = reductionofAFraction(f2);
-  var minCommonMultiple = getMinCommonMultiple(Math.abs(f1.denominator), Math.abs(f2.denominator));
+  var minCommonMultiple = lcm(Math.abs(f1.denominator), Math.abs(f2.denominator));
   f[0] = { numerator: f1.numerator, denominator: minCommonMultiple };
   f[1] = { numerator: f2.numerator, denominator: minCommonMultiple };
   f[0].denominator = f[1].denominator = minCommonMultiple;
